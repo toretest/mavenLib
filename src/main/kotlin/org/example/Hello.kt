38d3071b03a6b691c531cda1ef667b73195cb2ae
tore gard <dragerot@gmail.com>
2022-03-02 01:21:17 +0100

    Initial commit

---
+++ src/main/kotlin/org/example/Hello.kt
@@ -0,0 +1,17 @@
+package org.example
+
+fun main(args: Array<String>) {
+A.runMe()
+}
+
+class A {
+    companion object {
+        fun runMe() = println("Aggressive")
+    }
+}
+
+
+
+
+
+
---
+++ src/main/kotlin/org/example/TryMe.kts
@@ -0,0 +1,15 @@
+import com.squareup.kotlinpoet.*
+import org.example.A
+import org.example.underpakke.HovedKjoring
+
+/**
+ * https://search.maven.org/artifact/org.jetbrains.kotlin/kotlin-main-kts
+ * https://square.github.io/kotlinpoet/#example
+ */
+
+
+//println("Hello")
+//A.runMe()
+println("*******************Genrate kode:")
+HovedKjoring().kjorHoved()
+
